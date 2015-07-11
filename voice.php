b80392ccc8b04d6e357b211f080d1eaac08c839f
Alexandre G.-Raymond <alex@ndre.gr>
2015-07-11 21:10:47 +0200

    Fix security holes & bugs in voice.php

--- voice.php
+++ voice.php
@@ -3,41 +3,51 @@
 $base_dir = "/tmp/";
 
 // get input parameters
-$text = $_GET['text'];
+$text = escapeshellarg($_GET['text']);
 if (strlen($text) > 100) {
     $text = "";
 }
 
 if (isset($_GET['voice'])) {
-  $voice = $_GET['voice'];
+    $voice = escapeshellarg($_GET['voice']);
 } else {
-  $voice = 'en';
+    $voice = 'en';
 }
 
 if (isset($_GET['speed'])) {
-  $speed = $_GET['speed'];
+    $speed = (int)$_GET['speed'];
 } else {
-  $speed = 175;
+    $speed = 175;
 }
 
 if (isset($_GET['pitch'])) {
-  $pitch = $_GET['pitch'];
+    $pitch = (int)$_GET['pitch'];
 } else {
-  $pitch = 50;
+    $pitch = 50;
 }
 
 $volume = 100;
 
-$filename = md5($text) . '.mp3';
-$filepath = $base_dir . 'v' . $voice . 's' . $speed . 'p' . $pitch .
-    'a' . $volume . 't' . $filename;
+$filename = md5($text . $voice . $speed . $pitch) . '.mp3';
+$filepath = $base_dir . $filename;
 
-$text = escapeshellarg($text);
 $cmd = "espeak -v $voice -s $speed -p $pitch -a $volume --stdout $text --punct=.,:/|\
-lame --preset voice -q 9 --vbr-new - $filepath";
+lame --silent --preset voice -q 9 --vbr-new - $filepath";
 exec($cmd);
 
+$clean_file = false;
+if (file_exists($filepath)) {
+    $clean_file = true;
+}
+else {
+    $filepath = __DIR__."/blank.mp3";
+}
+$filesize = filesize($filepath);
+
 header('Content-Type: audio/mpeg');
-header('Content-Length: '.filesize($filepath));
+header('Content-Length: '.$filesize);
 readfile($filepath);
-unlink($filepath);
+
+if ($clean_file) {
+    unlink($filepath);
+}
